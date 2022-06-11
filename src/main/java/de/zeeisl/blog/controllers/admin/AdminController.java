6a38f4e96d9896cb1f207eb2193f2aa7b8a26724
Zeeshan I <kontakt@zeeshan-islam.de>
2022-06-11 15:53:54 +0200

    impl. login-as-user for admin

--- src/main/java/de/zeeisl/blog/controllers/admin/AdminController.java
+++ src/main/java/de/zeeisl/blog/controllers/admin/AdminController.java
@@ -1,8 +1,22 @@
 package de.zeeisl.blog.controllers.admin;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import de.zeeisl.blog.entities.User;
+import de.zeeisl.blog.repositories.UserRepository;
 
 @Controller
 @RequestMapping("/admin")
@@ -12,5 +26,4 @@ public class AdminController {
     String home() {
         return "admin/home";
     }
-    
 }
--- src/main/java/de/zeeisl/blog/controllers/admin/AdminUserController.java
+++ src/main/java/de/zeeisl/blog/controllers/admin/AdminUserController.java
@@ -3,9 +3,15 @@ package de.zeeisl.blog.controllers.admin;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import de.zeeisl.blog.entities.User;
 import de.zeeisl.blog.repositories.UserRepository;
@@ -25,5 +31,16 @@ public class AdminUserController {
 
         return "admin/users/index";
     }
-    
+
+    @GetMapping("/admin/loginAs/{id}")
+    String loginAsUserTest(@PathVariable(name = "id", required = true) Long id) {
+        User user = userRepository.findById(id).get();
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user, null,
+                AuthorityUtils.createAuthorityList(user.getUserRole()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return "redirect:/users/" + user.getId();
+    }
+
 }
